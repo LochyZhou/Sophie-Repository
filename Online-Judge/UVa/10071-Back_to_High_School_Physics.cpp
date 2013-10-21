056f349664d16ab36cba3829b5babe2fa5745b4b
LochyZhou <791240265@qq.com>
2013-10-21 16:15:16 +0800

    2013-10-15 Back to High School Physics: first commit

---
+++ Online-Judge/UVa/10071-Back_to_High_School_Physics.cpp
@@ -0,0 +1,18 @@
+/*
+Problem:10071 - Back to High School Physics
+Date: 2013-10-15
+Author:Sophie
+Compiler:GNU GCC
+Lang:c++
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    while(scanf("%d %d",&a,&b) ==2 )
+    {
+        printf("%d\n",2*a*b);
+    }
+    return 0;
+}
